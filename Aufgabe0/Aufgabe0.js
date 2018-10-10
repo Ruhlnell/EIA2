ac7253481039e152bd76750ad1598eebe66fa1c5
Nelli <Nelli@Sweety.fritz.box>
2018-10-10 14:06:19 +0200

    Typ geändert

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,8 +1,9 @@
 var a = prompt('Hallo, wie heisst du?');
 console.log("Hallo " + a);
+
 function writeHTML() {
-    var node = document.getElementById("main");
-    node.innerHTML += ("Hallo " + a);
+	var node = document.getElementById("main");
+	node.innerHTML += ("Hallo " + a);
 }
 document.addEventListener('DOMContentLoaded', writeHTML);
-//# sourceMappingURL=Aufgabe0.js.map
+// # sourceMappingURL=Aufgabe0.js.map
--- Aufgabe0/Aufgabe0.ts
+++ Aufgabe0/Aufgabe0.ts
@@ -1,11 +1,11 @@
-        var a: string= prompt ('Hallo, wie heisst du?');
+var a: string = prompt('Hallo, wie heisst du?');
 
-            console.log ("Hallo " + a);
+console.log("Hallo " + a);
 
-    function writeHTML() : void {
-        
-        var node : any = document.getElementById("main");
-        node.innerHTML += ("Hallo " + a);
-        }
+function writeHTML(): void {
+
+    var node: HTMLElement = document.getElementById("main");
+    node.innerHTML += ("Hallo " + a);
+}
 
 document.addEventListener('DOMContentLoaded', writeHTML);
