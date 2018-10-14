f0e3ebf6b596fdbcebbcb96d8fb97ce77f2357dd
Nelli <Nelli@Sweety.fritz.box>
2018-10-14 18:34:52 +0200

    A1.2

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,8 +1,11 @@
-var a = prompt('Hallo, wie heisst du?');
-console.log("Hallo " + a);
-function writeHTML() {
-	var node = document.getElementById("main");
-	node.innerHTML += ("Hallo " + a);
-}
-document.addEventListener('DOMContentLoaded', writeHTML);
-// # sourceMappingURL=Aufgabe0.js.map
+var Aufgabe0;
+(function (Aufgabe0) {
+    var a = prompt('Hallo, wie heisst du?');
+    console.log("Hallo " + a);
+    function writeHTML() {
+        var node = document.getElementById("main");
+        node.innerHTML += ("Hallo " + a);
+    }
+    document.addEventListener('DOMContentLoaded', writeHTML);
+})(Aufgabe0 || (Aufgabe0 = {}));
+//# sourceMappingURL=Aufgabe0.js.map
