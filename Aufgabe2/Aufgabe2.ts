a8697c98615d9593e4ded3d4530689f531f8432f
Nelli <Nelli@Sweety.fritz.box>
2018-10-28 16:04:33 +0100

    A2

--- Aufgabe2/Aufgabe2.ts
+++ Aufgabe2/Aufgabe2.ts
@@ -1,3 +1,10 @@
+//Aufgabe: 2 (neu)
+//Name: Ruhl, Nelli
+//Matrikel: 259408
+//Datum: 28.10.2018
+    
+//Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+
 namespace UNO {
     interface cards {
         color: string;
