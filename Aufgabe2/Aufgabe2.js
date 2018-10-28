2a1db09894d446d12c485b128cfe50d5cc780409
Nelli <Nelli@Sweety.fritz.box>
2018-10-28 16:01:37 +0100

    UNO überarbeitet

--- Aufgabe2/Aufgabe2.js
+++ Aufgabe2/Aufgabe2.js
@@ -1,235 +1,36 @@
 var UNO;
 (function (UNO) {
-    function game() {
-        let c0 = {
-            name: "0",
-            red: 1,
-            blue: 1,
-            green: 1,
-            yellow: 1,
-            black: 0,
-        };
-        let c1 = {
-            name: "1",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c2 = {
-            name: "2",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c3 = {
-            name: "3",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c4 = {
-            name: "4",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c5 = {
-            name: "5",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c6 = {
-            name: "6",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c7 = {
-            name: "7",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c8 = {
-            name: "8",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c9 = {
-            name: "9",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c10 = {
-            name: "Retour",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c11 = {
-            name: "+2",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c12 = {
-            name: "Aussetzen",
-            red: 2,
-            blue: 2,
-            green: 2,
-            yellow: 2,
-            black: 0,
-        };
-        let c13 = {
-            name: "+4",
-            red: 0,
-            blue: 0,
-            green: 0,
-            yellow: 0,
-            black: 4,
-        };
-        let c14 = {
-            name: "Wish",
-            red: 0,
-            blue: 0,
-            green: 0,
-            yellow: 0,
-            black: 4,
-        };
-        let c = ""; //Farbe
-        let allCards = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14];
-        function random(_n) {
-            return Math.floor(Math.random() * Math.floor(_n));
-        }
-        function placeDiv(_color, card, Eingabe) {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "a" + Eingabe);
-            document.getElementById("a" + Eingabe).innerHTML += card;
-            //CSS 
-            let s = div.style;
-            s.border = "thin solid black";
-            s.position = "absolute";
-            s.backgroundColor = _color;
-            s.width = 100 + "px";
-            s.height = 150 + "px";
-            s.left = (Eingabe + 1) * 150 + "px";
-            s.bottom = 50 + "px";
-            s.textAlign = "center";
-            s.borderRadius = 20 + "px";
-            if (_color == "black") {
-                s.color = "white";
+    //Arrays
+    let colors = ["red", "blue", "green", "yellow"];
+    let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "x", "<->"];
+    let other = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" },
+        { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" },
+        { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }];
+    for (let i = 0; i < colors.length; i++) {
+        for (let a = 0; a < values.length; a++) {
+            for (let z = 0; z < 2; z++) {
+                let card = { color: colors[i], value: values[a] };
+                other.push(card);
             }
         }
-        //Pop up
-        let Anzahl;
-        let Kartenauswahl = prompt("Mit wie vielen Karten spielst du?");
-        Anzahl = Number(Kartenauswahl);
-        for (let i = 0; i < Anzahl; i++) {
-            let l = random(15);
-            let farben = random(5);
-            switch (farben) {
-                case 0:
-                    c = "#FF0000";
-                    if (allCards[l].red > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].red--;
-                        continue;
-                    }
-                case 1:
-                    c = "#00FF00";
-                    if (allCards[l].green > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].green--;
-                        continue;
-                    }
-                case 2:
-                    c = "#0000FF";
-                    if (allCards[l].blue > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].blue--;
-                        continue;
-                    }
-                case 3:
-                    c = "#F0000F";
-                    if (allCards[l].yellow > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].yellow--;
-                        continue;
-                    }
-                case 4:
-                    c = "black";
-                    if (allCards[l].black > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].black--;
-                        continue;
-                    }
-                    else {
-                        i--;
-                        continue;
-                    }
-            }
-        }
-        function ablage() {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Ablage");
-            document.getElementById("Ablage").innerHTML += "Ablage";
-            let s = div.style;
-            s.border = "solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "white";
-            s.width = 100 + "px";
-            s.height = 150 + "px";
-            s.right = 200 + "px";
-            s.top = 20 + "px";
-            s.borderRadius = 20 + "px";
-        }
-        function stapel() {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Stapel");
-            document.getElementById("Stapel").innerHTML += "Stapel";
-            let s = div.style;
-            s.border = " solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "black";
-            s.width = 100 + "px";
-            s.height = 150 + "px";
-            s.left = 200 + "px";
-            s.top = 20 + "px";
-            s.borderRadius = 20 + "px";
-        }
-        ablage();
-        stapel();
     }
-    document.addEventListener("DOMContentLoaded", (game));
+    console.log(other);
+    let displayhand = [];
+    function Anzahl() {
+        var kartenanzahl = prompt("Mit wie vielen Karten willst du spielen?");
+        let n = parseInt(kartenanzahl);
+        for (let random = n; random > 0; random--) {
+            let r = Math.floor(Math.random() * (other.length - 1));
+            displayhand.push(other[r]);
+            other.splice(r, 1);
+        }
+        for (let d = 0; d < displayhand.length; d++) {
+            let div = document.createElement("div");
+            document.getElementById("displayhand").appendChild(div);
+            div.innerHTML = displayhand[d].value;
+            div.classList.add(displayhand[d].color);
+        }
+    }
+    document.addEventListener("DOMContentLoaded", Anzahl);
 })(UNO || (UNO = {}));
 //# sourceMappingURL=Aufgabe2.js.map
--- Aufgabe2/Aufgabe2.ts
+++ Aufgabe2/Aufgabe2.ts
@@ -1,293 +1,46 @@
 namespace UNO {
-    function game(){
     interface cards {
-        name: string;
-        red: number;
-        blue: number;
-        green: number;
-        yellow: number;
-        black: number;
+        color: string;
+        value: string;
     }
-    let c0: cards = {
-        name: "0",
-        red: 1,
-        blue: 1,
-        green: 1,
-        yellow: 1,
-        black: 0,
-    }
+//Arrays
+    let colors: string[] = ["red", "blue", "green", "yellow"];
+    let values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "x", "<->"];
+    let other: cards[] = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" },
+        { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" },
+        { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }];
 
-    let c1: cards = {
-        name: "1",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c2: cards = {
-        name: "2",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c3: cards = {
-        name: "3",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c4: cards = {
-        name: "4",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c5: cards = {
-        name: "5",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c6: cards = {
-        name: "6",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c7: cards = {
-        name: "7",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c8: cards = {
-        name: "8",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c9: cards = {
-        name: "9",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c10: cards = {
-        name: "Retour",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c11: cards = {
-        name: "+2",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-
-    let c12: cards = {
-        name: "Aussetzen",
-        red: 2,
-        blue: 2,
-        green: 2,
-        yellow: 2,
-        black: 0,
-    }
-    
-    let c13: cards = {
-        name: "+4",
-        red: 0,
-        blue: 0,
-        green: 0,
-        yellow: 0,
-        black: 4,
-    }
-    
-    let c14: cards = {
-        name: "Wish",
-        red: 0,
-        blue: 0,
-        green: 0,
-        yellow: 0,
-        black: 4,
-    }
-
-    let c: string = ""; //Farbe
-
-    let allCards = [c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14]
-
-function random(_n: number): number {
-        return Math.floor(Math.random() * Math.floor(_n));
-    }
-
-        function placeDiv(_color: string, card: string, Eingabe: number): void {
-            let div: HTMLDivElement = document.createElement("div");
-            document.body.appendChild(div);
-
-            div.setAttribute("id", "a" + Eingabe);
-            document.getElementById("a" + Eingabe).innerHTML += card;
-            
-            //CSS 
-            let s: CSSStyleDeclaration = div.style;
-            s.border = "thin solid black";
-            s.position = "absolute";
-            s.backgroundColor = _color;
-            s.width = 100 + "px";
-            s.height = 150 + "px";
-            s.left = (Eingabe + 1) * 150 + "px"; 
-            s.bottom = 50 + "px";
-            s.textAlign = "center";
-            s.borderRadius = 20 + "px";
-
-        if (_color == "black") { 
-            s.color = "white";
-
-        }
-
-        }
-
-        //Pop up
-        let Anzahl: number;
-        let Kartenauswahl: string = prompt("Mit wie vielen Karten spielst du?");
-        Anzahl = Number(Kartenauswahl);
-    
-        for (let i : number = 0; i < Anzahl; i++) { 
-
-
-            let l: number = random(15); 
-            let farben: number = random(5);
-
-            switch (farben) {
-
-
-
-                case 0:
-                    c = "#FF0000";
-                    if (allCards[l].red > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].red--;
-                        continue;
-                    }
-
-
-                case 1:
-
-
-                    c = "#00FF00";
-                    if (allCards[l].green > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].green--;
-                        continue;
-                    }
-
-                    
-                case 2:
-                    c = "#0000FF";
-                    if (allCards[l].blue > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].blue--;
-                        continue;
-                    }
-
-
-
-
-                case 3:
-                    c = "#F0000F";
-                    if (allCards[l].yellow > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].yellow--;
-                        continue;
-                    }
-
-
-                case 4:
-                    c = "black";
-                    if (allCards[l].black > 0) {
-                        placeDiv(c, allCards[l].name, i);
-                        allCards[l].black--;
-                        continue;
-                    }
-
-                    else {
-                        i--;
-                        continue
-                    }
+    for (let i: number = 0; i < colors.length; i++) {
+        for (let a: number = 0; a < values.length; a++) {
+            for (let z: number = 0; z < 2; z++) {
+                let card: cards = { color: colors[i], value: values[a] };
+                other.push(card);
             }
         }
+    }
+    console.log(other);
+
+    let displayhand: cards[] = [];
+
+    function Anzahl(): void {
+        var kartenanzahl: string = prompt("Mit wie vielen Karten willst du spielen?");
+        let n: number = parseInt(kartenanzahl);
 
 
-      
-        function ablage() {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Ablage");
-            document.getElementById("Ablage").innerHTML += "Ablage";
-            let s = div.style;
-            s.border = "solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "white";
-            s.width = 100 + "px";
-            s.height = 150 + "px";
-            s.right = 200 + "px";
-            s.top = 20 + "px";
-            s.borderRadius = 20 + "px";
-
+        for (let random: number = n; random > 0; random--) {
+            let r: number = Math.floor(Math.random() * (other.length - 1));
+            displayhand.push(other[r]);
+            other.splice(r, 1);
         }
-        
-        function stapel() {
-            let div = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("id", "Stapel");
-            document.getElementById("Stapel").innerHTML += "Stapel";
-            let s = div.style;
-            s.border = " solid black";
-            s.textAlign = "center";
-            s.position = "absolute";
-            s.backgroundColor = "black";
-            s.width = 100 + "px";
-            s.height = 150 + "px";
-            s.left = 200 + "px";
-            s.top = 20 + "px";
-            s.borderRadius = 20 + "px";
 
+        for (let d: number = 0; d < displayhand.length; d++) {
+            let div: HTMLElement = document.createElement("div");
+            document.getElementById("displayhand").appendChild(div);
+            div.innerHTML = displayhand[d].value;
+            div.classList.add(displayhand[d].color);
+        }
 
     }
 
-        ablage();
-        stapel();
-}  
-    document.addEventListener("DOMContentLoaded", (game))
+    document.addEventListener("DOMContentLoaded", Anzahl);
 }
