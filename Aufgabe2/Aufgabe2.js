89782244aecf4ea442a993ca72edf9d16d9ff4c9
Nelli <Nelli@Sweety.fritz.box>
2018-11-07 16:44:39 +0100

    konnzept

--- Aufgabe2/Aufgabe2.js
+++ Aufgabe2/Aufgabe2.js
@@ -8,14 +8,14 @@ var UNO;
     //Arrays
     let colors = ["red", "blue", "green", "yellow"];
     let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "x", "<->"];
-    let other = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" },
+    let deck = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" },
         { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" },
         { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }];
     for (let i = 0; i < colors.length; i++) {
         for (let a = 0; a < values.length; a++) {
             for (let z = 0; z < 2; z++) {
                 let card = { color: colors[i], value: values[a] };
-                other.push(card);
+                deck.push(card);
             }
         }
     }
@@ -24,9 +24,9 @@ var UNO;
         var kartenanzahl = prompt("Mit wie vielen Karten willst du spielen?");
         let n = parseInt(kartenanzahl);
         for (let random = n; random > 0; random--) {
-            let r = Math.floor(Math.random() * (other.length - 1));
-            displayhand.push(other[r]);
-            other.splice(r, 1);
+            let r = Math.floor(Math.random() * (deck.length - 1));
+            displayhand.push(deck[r]);
+            deck.splice(r, 1);
         }
         for (let d = 0; d < displayhand.length; d++) {
             let div = document.createElement("div");
--- Aufgabe2/Aufgabe2.ts
+++ Aufgabe2/Aufgabe2.ts
@@ -6,27 +6,27 @@
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 
 namespace UNO {
-    interface cards {
+    interface card {
         color: string;
         value: string;
     }
 //Arrays
     let colors: string[] = ["red", "blue", "green", "yellow"];
     let values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "x", "<->"];
-    let other: cards[] = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" },
+    let deck: card[] = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" },
         { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" },
         { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }];
 
     for (let i: number = 0; i < colors.length; i++) {
         for (let a: number = 0; a < values.length; a++) {
             for (let z: number = 0; z < 2; z++) {
-                let card: cards = { color: colors[i], value: values[a] };
-                other.push(card);
+                let card: card = { color: colors[i], value: values[a] };
+                deck.push(card);
             }
         }
     }
 
-    let displayhand: cards[] = [];
+    let displayhand: card[] = [];
 
     function Anzahl(): void {
         var kartenanzahl: string = prompt("Mit wie vielen Karten willst du spielen?");
@@ -34,9 +34,9 @@ namespace UNO {
 
 
         for (let random: number = n; random > 0; random--) {
-            let r: number = Math.floor(Math.random() * (other.length - 1));
-            displayhand.push(other[r]);
-            other.splice(r, 1);
+            let r: number = Math.floor(Math.random() * (deck.length - 1));
+            displayhand.push(deck[r]);
+            deck.splice(r, 1);
         }
 
         for (let d: number = 0; d < displayhand.length; d++) {
