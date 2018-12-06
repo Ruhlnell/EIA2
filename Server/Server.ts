69d2f094232329b121753587d6d1b7b3e36b2c05
nelli <nelli@LAPTOP-QLN2NTKD.eduroam.hs-furtwangen.de>
2018-12-06 09:36:25 +0100

    Server.ts

--- Server/Server.ts
+++ Server/Server.ts
@@ -1,29 +1,48 @@
 import * as Http from "http"; //Import eines Modul als HTTP Objekt
-
+import * as Url from "url";
 namespace L06_SendData {
     console.log("Starting server"); //Consolelog wird ausgegeben mit nachricht "Starting server"
     let port: number = process.env.PORT; //Stellt den PORT als number ein zum Heroku server  
     if (port == undefined) //Falls der Port nicht definiert ist,
         port = 8100; //soll er die nummer 8100 haben (verbindet Heroku mit EIA2)
 
+    let item: string[] = [];
     let server: Http.Server = Http.createServer(); //variable server wird als typ Http.Server und wird als Http.creatserver funktion gleichgesetzt
     server.addListener("request", handleRequest); //Fügt einen Listener hinzu der dem händler bescheid gibt wenn handleRequest getriggert wurde
     server.addListener("listening", handleListen); //solange der Käufer auf die funktion zugreift, sieht der Händler das.
     server.listen(port); //schaut zu was für ein Port verwendet wird. (8100)
 
-    function handleListen(): void {                 
+    function handleListen(): void {
         console.log("Listening"); //Consolelog "Listening" wird ausgegeben
     }
 
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { //erstellt eine Funktion die bei einer Http.Incomingmessage, eine Http.ServerResponse zurück schickt
-    //console.log("I hear voices")      // gibt Consolelog "I hear voices!" aus
+        //console.log("I hear voices")      // gibt Consolelog "I hear voices!" aus
         console.log(_request.url);
-        
+
         _response.setHeader("content-type", "text/html; charset=utf-8"); //setzt in den HTML header "content-type" und text/html; charset=utf-8
         _response.setHeader("Access-Control-Allow-Origin", "*"); //Access-Control-Allow-Origin wird auch in den Header gesetzt, damit die antwort des Codes mit dem Nutzer geteilt wird.
 
-        _response.write(_request.url); //gibt bei _response.write eine angefragte URL aus   
-        
-        _response.end(); //beendet die schleife der _response
+        let url: string = _request.url;
+        if (url != "/favicon.ico") {
+            let paragraph: string = Url.parse(url).search.substr(1);
+            let childNodeHTML: string = "";
+            for (let i: number = 0; i < paragraph.length; i++) {
+                if (paragraph[i] == "&") {
+                    item.push(childNodeHTML);
+                    childNodeHTML = "<br>";
+                }
+                else {
+                    childNodeHTML += paragraph[i];
+                }
+            }
+            item.push(childNodeHTML);
+
+            for (let i: number = 0; i < item.length; i++) {
+                _response.write(item[i]);
+            }
+            console.log(item);
+        }
+        _response.end();
     }
 }
